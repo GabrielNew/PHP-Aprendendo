4d38041d3aac5ef659a1d6d67bc4f5b7784e99ca
Gabriel Antonio <gabrielfai70@gmail.com>
2019-12-16 07:18:37 -0300

    Variáveis variáveis

---
+++ Básico/aula03.php
@@ -0,0 +1,9 @@
+<?php
+// Variáveis variáveis
+
+$moto = "Marca"; // Aqui declaramos o nome da variável
+$$moto = "Hornet"; // Já neste ponto, declaramos o valor armazenado na variável
+
+echo "A $moto da moto é $Marca.";
+
+?>
