e8ae5f6ceaf8964363ff9ef6ebaf1ff1cbcb3b54
Gabriel Antonio <gabrielfai70@gmail.com>
2019-12-24 15:39:01 -0300

    Aula01 removida

--- Básico/aula1.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Usando do echo e comentário de uma linha
-    echo "Usando o echo para imprimir uma mensagem <br/> ";
-
-    /* Usando o print
-    e
-    comentário de várias linhas */
-    print "Usando o print para imprimir uma mensagem";
-?>
