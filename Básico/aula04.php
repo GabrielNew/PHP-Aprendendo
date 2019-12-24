e0a9c2a1167dc3fb43180b7371c730591aca77f6
Gabriel Antonio <gabrielfai70@gmail.com>
2019-12-24 18:01:01 -0300

    Aspas simples e duplas

---
+++ Básico/aula04.php
@@ -0,0 +1,8 @@
+<?php
+    $nome = 'Gohan';
+
+    // echo 'Meu nome é $nome'; exibe na tela Meu nome é $nome pois aspas simples são literais
+    echo 'Meu nome é '.$nome; // o . aqui serve para concatenar
+    echo "<br>";
+    echo "Meu nome é $nome"; // neste caso a variável será interpretada
+?>
