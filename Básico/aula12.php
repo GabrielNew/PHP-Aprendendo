ecd748da65e922dce771c4a3cedb17840577c2dc
Gabriel Antonio <gabrielfai70@gmail.com>
2019-12-31 19:56:49 -0300

    Switch/Case

---
+++ Básico/aula12.php
@@ -0,0 +1,22 @@
+<?php
+
+$op = 1;
+
+// Switch/Case do PHP
+switch ($op) {
+    case 1:
+        echo "Cadastrar";
+        break;
+    case 2:
+        echo "Atualizar";
+        break;
+    case 3:
+        echo "Ler";
+        break;
+    case 4:
+        echo "Excluir";
+        break;
+    default:
+        echo "Opção Inválida";
+        break;
+}
