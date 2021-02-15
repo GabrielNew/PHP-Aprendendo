246339fc365e487e574247becff758db11026dc4
Gabriel Antonio <gabrielfai70@gmail.com>
2021-02-14 22:54:03 -0300

    Tabela Provisória

--- Laravel/introducao/resources/views/produtos/index.blade.php
+++ Laravel/introducao/resources/views/produtos/index.blade.php
@@ -3,9 +3,29 @@
 @section('content')
 <div class="container">
 <a href="produtos/inserir" type="button" class="mt-4 mb-4 btn btn-primary">Inserir Produto</a>
-    @foreach($produtos as $produto)
+<table>
+<thead>
+            <tr>
+                <th>Nome</th>
+                <th>Valor</th>
+                <th>Estoque</th>
+                <th>Ação</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($produtos as $produto)
+            <tr>
+                <td>{{$produto->nome}}</td>
+                <td>{{$produto->valor}}</td>
+                <td>{{$produto->estoque}}</td>
+                <td></td>
+            </tr>
+        @endforeach
+        </tbody>
+</table>
+    <!-- @foreach($produtos as $produto)
     <p>{{$produto->nome}}</p>
-    @endforeach
+    @endforeach -->
 
     {{$produtos->links()}}
 </div>
